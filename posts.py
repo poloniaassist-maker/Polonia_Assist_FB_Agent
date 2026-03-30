01b9a705b2f964efe467138caedf8543c00a3878
Polonia Assist <poloniaassist@gmail.com>
2026-03-30 18:40:28 +0200

    Update posts.py

--- posts.py
+++ posts.py
@@ -1,67 +1,8 @@
 import requests
-import os
 from datetime import datetime, timedelta
+from config import FB_PAGE_ID, FB_PAGE_TOKEN
 
-FB_PAGE_ID = os.getenv("FB_PAGE_ID")
-FB_PAGE_TOKEN = os.getenv("FB_PAGE_TOKEN")
 
-def get_recent_posts_without_pa_comment(group_id):
-    """
-    Pobiera posty z grupy:
-    - nie starsze niż 12 godzin
-    - bez komentarza Polonia Assist
-    """
-
-    # 1. Pobierz feed grupy
-    url = f"https://graph.facebook.com/v18.0/{group_id}/feed"
-    params = {
-        "access_token": FB_PAGE_TOKEN,
-        "limit": 25,  # wystarczy do 12h
-        "fields": "id,message,created_time"
-    }
-
-    response = requests.get(url, params=params)
-    data = response.json()
-
-    if "data" not in data:
-        return []
-
-    now = datetime.utcnow()
-    cutoff = now - timedelta(hours=12)
-
-    valid_posts = []
-
-    for post in data["data"]:
-        # --- 2. Filtr: post nie starszy niż 12 godzin ---
-        created = datetime.strptime(
-            post["created_time"], "%Y-%m-%dT%H:%M:%S%z"
-        ).replace(tzinfo=None)
-
-        if created < cutoff:
-            continue
-
-        # --- 3. Filtr: brak komentarza Polonia Assist ---
-        comments_url = f"https://graph.facebook.com/v18.0/{post['id']}/comments"
-        comments_params = {
-            "access_token": FB_PAGE_TOKEN,
-            "filter": "stream",
-            "fields": "from"
-        }
-
-        comments_res = requests.get(comments_url, params=comments_params).json()
-
-        already_commented = False
-
-        if "data" in comments_res:
-            for c in comments_res["data"]:
-                if c.get("from", {}).get("id") == FB_PAGE_ID:
-                    already_commented = True
-                    break
-
-        if not already_commented:
-            valid_posts.append(post)
-
-    return valid_posts
 def get_recent_posts_without_pa_comment():
     """
     Pobiera posty z FEEDU STRONY (FB_PAGE_ID):
@@ -81,6 +22,7 @@ def get_recent_posts_without_pa_comment():
     data = response.json()
 
     if "data" not in data:
+        print("Brak danych z feedu strony:", data)
         return []
 
     now = datetime.utcnow()
@@ -95,8 +37,7 @@ def get_recent_posts_without_pa_comment():
 
         try:
             created = datetime.strptime(created_raw, "%Y-%m-%dT%H:%M:%S%z").replace(tzinfo=None)
-        except ValueError:
-            # jeśli FB doda milisekundy itp. – pomijamy post
+        except Exception:
             continue
 
         if created < cutoff:
@@ -125,8 +66,7 @@ def get_recent_posts_without_pa_comment():
         valid_posts.append(post)
 
     return valid_posts
-import requests
-from config import FB_PAGE_TOKEN
+
 
 def send_comment(post_id, message):
     """
