c0c7b46504b8fba8c173a50ca562af6b6a7a6c6c
Polonia Assist <poloniaassist@gmail.com>
2026-03-30 18:50:38 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+FB_PAGE_ID = os.getenv("FB_PAGE_ID")
+FB_PAGE_TOKEN = os.getenv("FB_PAGE_TOKEN")
+
+# Możesz dodać inne zmienne środowiskowe, jeśli używasz:
+# OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
