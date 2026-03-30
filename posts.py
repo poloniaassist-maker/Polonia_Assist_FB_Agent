8895ed11a44a4f1bfb561fd7e271f38fe657ca9c
Polonia Assist <poloniaassist@gmail.com>
2026-03-30 18:28:37 +0200

    Update posts.py

--- posts.py
+++ posts.py
@@ -62,18 +62,66 @@ def get_recent_posts_without_pa_comment(group_id):
             valid_posts.append(post)
 
     return valid_posts
-def send_comment(post_id, message):
+def get_recent_posts_without_pa_comment():
     """
-    Wysyła komentarz jako strona Polonia Assist.
+    Pobiera posty z FEEDU STRONY (FB_PAGE_ID):
+    - nie starsze niż 12 godzin
+    - bez komentarza Polonia Assist
+    - mogą pochodzić z grup, w których strona jest członkiem
     """
-    url = f"https://graph.facebook.com/v18.0/{post_id}/comments"
+
+    url = f"https://graph.facebook.com/v18.0/{FB_PAGE_ID}/feed"
     params = {
         "access_token": FB_PAGE_TOKEN,
-        "message": message,
+        "limit": 50,
+        "fields": "id,message,created_time,permalink_url,from"
     }
 
-    response = requests.post(url, params=params)
-    try:
-        return response.json()
-    except ValueError:
-        return {"error": "Invalid JSON response", "status_code": response.status_code, "text": response.text}
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    if "data" not in data:
+        return []
+
+    now = datetime.utcnow()
+    cutoff = now - timedelta(hours=12)
+
+    valid_posts = []
+
+    for post in data["data"]:
+        created_raw = post.get("created_time")
+        if not created_raw:
+            continue
+
+        try:
+            created = datetime.strptime(created_raw, "%Y-%m-%dT%H:%M:%S%z").replace(tzinfo=None)
+        except ValueError:
+            # jeśli FB doda milisekundy itp. – pomijamy post
+            continue
+
+        if created < cutoff:
+            continue
+
+        # sprawdź, czy już komentowaliśmy ten post jako strona
+        comments_url = f"https://graph.facebook.com/v18.0/{post['id']}/comments"
+        comments_params = {
+            "access_token": FB_PAGE_TOKEN,
+            "filter": "stream",
+            "fields": "from"
+        }
+
+        comments_res = requests.get(comments_url, params=comments_params).json()
+
+        already_commented = False
+        if "data" in comments_res:
+            for c in comments_res["data"]:
+                if c.get("from", {}).get("id") == FB_PAGE_ID:
+                    already_commented = True
+                    break
+
+        if already_commented:
+            continue
+
+        valid_posts.append(post)
+
+    return valid_posts
